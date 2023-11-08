ae084c04451f4acc7c1fd6bf55840ff1f4fa2363
Vitalik <vitalik.yatsenko@jetup.team>
2023-11-09 00:34:59 +0200

    FIX | Env

--- src/config/access.config.ts
+++ src/config/access.config.ts
@@ -1,3 +1,5 @@
 export const accessConfig = {
-  API_URL: process.env.API_URL ? process.env.API_URL : "http://localhost:3000",
+  API_URL: process.env.NEXT_PUBLIC_API_URL
+    ? process.env.NEXT_PUBLIC_API_URL
+    : "http://localhost:3000",
 };
