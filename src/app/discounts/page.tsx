063d74fdcc1bb5519a46a87ab99e6d3921caed09
Vitalik <vitalik.yatsenko@jetup.team>
2023-11-30 23:25:22 +0200

    fix | Discounts

--- src/app/discounts/page.tsx
+++ src/app/discounts/page.tsx
@@ -1,14 +1,35 @@
+"use client";
+import React from "react";
 import { ProductsList } from "@/components/products-list";
 import { productsService } from "@/services/products.service";
+import { useEffect, useState } from "react";
 
-const CatalogPage = async () => {
-  const products = await productsService.getList({ withDiscount: true });
+const CatalogPage = () => {
+  const [products, setProducts] = useState<any[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const fetch = async () => {
+    setLoading(true);
+    try {
+      const products = await productsService.getList({ withDiscount: true });
+      setProducts(products);
+    } catch (e) {
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
 
   return (
     <div>
       <ProductsList
         items={products}
-        emptyMessage="Нажаль зараз немає знижок :("
+        emptyMessage={
+          isLoading ? "Завантаження..." : "Нажаль зараз немає знижок :("
+        }
       />
     </div>
   );
